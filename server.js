178cb56a3716c49476e8dd862ad823c67f3a0aa0
lifuzhao100 <lifuzhao1995@qq.com>
2018-06-06 01:59:49 +0800

    '优化'

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const Koa = require('koa');
-const static = require('koa-static');
-const compress = require('koa-compress');
+// const static = require('koa-static');
+const staticCache = require('koa-static-cache');
+// const compress = require('koa-compress');
+const { extname, join } = require('path');
 const app = new Koa();
-app.use(compress({
-	filter: function (content_type) {
-		return /\.(js|css|jpg|png|woff)(\.map)?$/i.test(content_type)
-	},
-	threshold: 2048,
-	flush: require('zlib').Z_SYNC_FLUSH
-}))
-app.use(static('build'));
+// app.use(compress({
+// 	filter: function (content_type) {
+// 		return true;
+// 	},
+// 	threshold: 10240,
+// 	flush: require('zlib').Z_SYNC_FLUSH
+// }))
+let files = {};
+app.use(staticCache(join(__dirname, 'build'), {
+	maxAge: 365 * 24 * 60 * 60,
+	gzip: true
+}, files));
+for (let file in files){
+	if(/\.html$/.test(file)){
+		files[file].maxAge = 0;
+	}
+}
+
 app.listen(7500);
