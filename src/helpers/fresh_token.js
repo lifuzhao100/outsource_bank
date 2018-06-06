231f95f96a71fce799d2b9a20d879f3888611d00
lifuzhao100 <lifuzhao1995@qq.com>
2018-06-06 22:28:03 +0800

    '测试新增预约接口'

--- src/helpers/fresh_token.js
+++ src/helpers/fresh_token.js
@@ -7,7 +7,7 @@ let getCode = () => {
 };
 let wxToken = (notForce, code) => {
 	// let origin = encodeURIComponent(location.href);
-	let origin = encodeURIComponent('http://bank.mengant.cn/index.html?code=123#/appointment/list');
+	let origin = encodeURIComponent('http://bank.mengant.cn/index.html#/');
 	let appid = 'wxf4b7d664b2461f4b';
 	let requestURL = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${origin}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect;`
 	//notForce true,上传code供后端获取用户信息 false,强制重新获取code
